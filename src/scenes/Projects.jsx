7c1b6505bf6104b8dee97a6c0374c41695d608d0
Gabrielshow <adefelashogbanmu@gmail.com>
2023-05-06 14:26:58 +0100

    addding existing files

---
+++ src/scenes/Projects.jsx
@@ -0,0 +1,116 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const images = [
+  { src: "../assets/project-1.jpeg", description: "Landing page" },
+  { src: "../assets/project-2.jpeg", description: "Blog" },
+  { src: "../assets/project-3.jpeg", description: "Funny Flakes" },
+  { src: "../assets/project-4.jpeg", description: "Minimalist design" },
+  { src: "../assets/project-5.jpeg", description: "Restaurant" },
+  { src: "../assets/project-6.jpeg", description: "Live Concert Booking app" },
+  { src: "../assets/project-7.jpeg", description: "React Admin Dashboard" }
+];
+
+const [image1, image2, image3, image4, image5, image6, image7] = images;
+
+
+const Project = ({ title, desc }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+          {desc}.
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+    </motion.div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      {/* HEADINGS */}
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+          <div className="flex justify-center mt-5">
+            <LineGradient width="w-2/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+          Here are some of the projects I have done so far.
+        </p>
+      </motion.div>
+
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+        >
+          {/* ROW 1 */}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-red
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            BEAUTIFUL USER INTERFACES
+          </div>
+          <Project title="Project 1" desc={image1.description}/>
+          <Project title="Project 2"  desc={image2.description} />
+
+          {/* ROW 2 */}
+          <Project title="Project 3" desc={image3.description}/>
+          <Project title="Project 4" desc={image4.description} />
+          <Project title="Project 5" desc={image5.description}/>
+
+          {/* ROW 3 */}
+          <Project title="Project 6" desc={image6.description}/>
+          <Project title="Project 7" desc={image7.description} />
+          <div
+            className="flex justify-center text-center items-center p-10 bg-blue
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
